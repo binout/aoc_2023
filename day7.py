4182359c3f595a69b53340b5e1d7d49719247283
binout <benoit.prioux@gmail.com>
2023-12-09 12:18:44 +0100

    day 7 part 2

--- day7.py
+++ day7.py
@@ -1,10 +1,12 @@
 from collections import Counter
 from enum import Enum
+from typing import List, Union
 
 from input_examples import retrieve_intput
 
 
 class Card(Enum):
+    JOKER = 0
     TWO = 2
     THREE = 3
     FOUR = 4
@@ -34,6 +36,13 @@ class Card(Enum):
         else:
             return Card(int(value))
 
+    @staticmethod
+    def parse_with_joker(value: str):
+        if value == "J":
+            return Card.JOKER
+        return Card.parse(value)
+
+
 class HandType(Enum):
     HIGH_CARD = 1
     PAIR = 2
@@ -44,12 +53,11 @@ class HandType(Enum):
     FIVE_OF_A_KIND = 7
 
 
+
 class Hand:
 
-    def __init__(self, input_text: str):
-        if len(input_text) != 5:
-            raise ValueError("Invalid number of cards")
-        self.cards = [Card.parse(input_card) for input_card in input_text]
+    def __init__(self, cards: List[Card]):
+          self.cards = cards
 
     def __repr__(self):
         return f"Hand({self.cards})"
@@ -69,21 +77,22 @@ class Hand:
         kinds = card_counter.most_common(2)
         highest_kind = max(kinds, key=lambda x: x[1])
         if highest_kind[1] == 5:
-            return HandType.FIVE_OF_A_KIND
+            result = HandType.FIVE_OF_A_KIND
         elif highest_kind[1] == 4:
-            return HandType.FOUR_OF_A_KIND
+            result = HandType.FOUR_OF_A_KIND
         elif highest_kind[1] == 3:
             if len(kinds) == 2 and kinds[1][1] == 2:
-                return HandType.FULL_HOUSE
+                result = HandType.FULL_HOUSE
             else:
-                return HandType.THREE_OF_A_KIND
+                result = HandType.THREE_OF_A_KIND
         elif highest_kind[1] == 2:
             if len(kinds) == 2 and kinds[1][1] == 2:
-                return HandType.DOUBLE_PAIR
+                result = HandType.DOUBLE_PAIR
             else:
-                return HandType.PAIR
+                result = HandType.PAIR
         else:
-            return HandType.HIGH_CARD
+            result = HandType.HIGH_CARD
+        return result
 
 
 def _default_comparator(hand1: Hand, hand2: Hand)->bool:
@@ -92,9 +101,67 @@ def _default_comparator(hand1: Hand, hand2: Hand)->bool:
     return first_diff[0].value > first_diff[1].value
 
 
+def parse_hand(input_text: str) -> "Hand":
+    if len(input_text) != 5:
+        raise ValueError("Invalid number of cards")
+    return Hand([Card.parse(input_card) for input_card in input_text])
+
+
+class HandWithJoker(Hand):
+
+    @property
+    def hand_type(self) -> HandType:
+        number_jokers = sum(card == Card.JOKER for card in self.cards)
+        counter = Counter(card for card in self.cards if card != Card.JOKER)
+        two_most_commons = counter.most_common(2)
+        hightest_kind = two_most_commons[0][1] if len(two_most_commons) > 0 else 0
+        second_highest_kind = two_most_commons[1][1] if len(two_most_commons) > 1 else 0
+
+        if number_jokers in [4, 5]:
+            return HandType.FIVE_OF_A_KIND
+        elif number_jokers == 3:
+            if len(set(self.cards)) == 2:
+                return HandType.FIVE_OF_A_KIND
+            else:
+                return HandType.FOUR_OF_A_KIND
+        elif number_jokers == 2:
+            if hightest_kind == 3:
+                return HandType.FIVE_OF_A_KIND
+            elif hightest_kind == 2:
+                return HandType.FOUR_OF_A_KIND
+            else:
+                return HandType.THREE_OF_A_KIND
+        elif number_jokers == 1:
+            if hightest_kind == 4:
+                return HandType.FIVE_OF_A_KIND
+            elif hightest_kind == 3:
+                return HandType.FOUR_OF_A_KIND
+            elif hightest_kind == 2:
+                if second_highest_kind == 2:
+                    return HandType.FULL_HOUSE
+                return HandType.THREE_OF_A_KIND
+            else:
+                return HandType.PAIR
+        else:
+            return Hand(self.cards).hand_type
+
+
+def parse_hand_with_joker(input_text: str) -> HandWithJoker:
+    if len(input_text) != 5:
+        raise ValueError("Invalid number of cards")
+    return HandWithJoker([Card.parse_with_joker(input_card) for input_card in input_text])
+
+
 def total_winnings(input_text:str):
     lines = [line for line in input_text.splitlines() if line.strip() != ""]
-    hands_with_bet = {Hand(line.split()[0]): int(line.split()[1]) for line in lines}
+    hands_with_bet = {parse_hand(line.split()[0]): int(line.split()[1]) for line in lines}
+    sorted_hands_with_bet = dict(sorted(hands_with_bet.items()))
+    return sum([(index+1) * sorted_hands_with_bet[hand] for index,hand in enumerate(sorted_hands_with_bet)])
+
+
+def total_winnings_with_joker(input_text:str):
+    lines = [line for line in input_text.splitlines() if line.strip() != ""]
+    hands_with_bet = {parse_hand_with_joker(line.split()[0]): int(line.split()[1]) for line in lines}
     sorted_hands_with_bet = dict(sorted(hands_with_bet.items()))
     return sum([(index+1) * sorted_hands_with_bet[hand] for index,hand in enumerate(sorted_hands_with_bet)])
 
@@ -102,3 +169,5 @@ def total_winnings(input_text:str):
 if __name__ == "__main__":
     input_text = retrieve_intput(day=7)
     print(f"total_winnings={total_winnings(input_text)}")
+    print(f"total_winnings_with_joker={total_winnings_with_joker(input_text)}")
+
--- test/test_day7.py
+++ test/test_day7.py
@@ -1,4 +1,5 @@
-from day7 import Card, Hand, total_winnings, HandType
+from day7 import Card, Hand, total_winnings, HandType, HandWithJoker, parse_hand, parse_hand_with_joker, \
+    total_winnings_with_joker
 
 
 def test_card():
@@ -7,49 +8,83 @@ def test_card():
 
 
 def test_hand():
-    hand = Hand("32T3K")
+    hand = parse_hand("32T3K")
     assert [Card.THREE, Card.TWO, Card.TEN, Card.THREE, Card.KING] == hand.cards
 
 def test_hand_type():
-    assert Hand("32T3K").hand_type == HandType.PAIR
-    assert Hand("T55J5").hand_type == HandType.THREE_OF_A_KIND
-    assert Hand("KK677").hand_type == HandType.DOUBLE_PAIR
-    assert Hand("KTJJT").hand_type == HandType.DOUBLE_PAIR
-    assert Hand("QQQJQ").hand_type == HandType.FOUR_OF_A_KIND
-    assert Hand("AAAAA").hand_type == HandType.FIVE_OF_A_KIND
+    assert parse_hand("32T3K").hand_type == HandType.PAIR
+    assert parse_hand("T55J5").hand_type == HandType.THREE_OF_A_KIND
+    assert parse_hand("KK677").hand_type == HandType.DOUBLE_PAIR
+    assert parse_hand("KTJJT").hand_type == HandType.DOUBLE_PAIR
+    assert parse_hand("QQQJQ").hand_type == HandType.FOUR_OF_A_KIND
+    assert parse_hand("AAAAA").hand_type == HandType.FIVE_OF_A_KIND
 
 def test_hand_comparator():
-    assert Hand("QQQJA") > Hand("KTJJT")
-    assert Hand("QQQJA") > Hand("KK677")
-    assert Hand("QQQJA") > Hand("T55J5")
-    assert Hand("QQQJA") > Hand("32T3K")
+    assert parse_hand("QQQJA") > parse_hand("KTJJT")
+    assert parse_hand("QQQJA") > parse_hand("KK677")
+    assert parse_hand("QQQJA") > parse_hand("T55J5")
+    assert parse_hand("QQQJA") > parse_hand("32T3K")
 
-    assert Hand("T55J5") > Hand("KTJJT")
-    assert Hand("T55J5") > Hand("KK677")
-    assert Hand("T55J5") > Hand("32T3K")
+    assert parse_hand("T55J5") > parse_hand("KTJJT")
+    assert parse_hand("T55J5") > parse_hand("KK677")
+    assert parse_hand("T55J5") > parse_hand("32T3K")
 
-    assert Hand("KK677") > Hand("32T3K")
-    assert Hand("KK677") > Hand("KTJJT")
+    assert parse_hand("KK677") > parse_hand("32T3K")
+    assert parse_hand("KK677") > parse_hand("KTJJT")
 
-    assert Hand("KTJJT") > Hand("32T3K")
+    assert parse_hand("KTJJT") > parse_hand("32T3K")
 
-    assert Hand("J2T2Q") > Hand("42836")
-    assert Hand("644J6") > Hand("54566")
+    assert parse_hand("J2T2Q") > parse_hand("42836")
+    assert parse_hand("644J6") > parse_hand("54566")
 
-    assert Hand("33332") > Hand("2AAAA")
-    assert Hand("77888") > Hand("77788")
+    assert parse_hand("33332") > parse_hand("2AAAA")
+    assert parse_hand("77888") > parse_hand("77788")
 
-    assert Hand("88773") > Hand("88772")
-    assert Hand("88883") > Hand("88882")
-    assert Hand("38888") > Hand("28888")
+    assert parse_hand("88773") > parse_hand("88772")
+    assert parse_hand("88883") > parse_hand("88882")
+    assert parse_hand("38888") > parse_hand("28888")
 
-    assert Hand("23457") > Hand("23456")
-    assert Hand("23452") > Hand("A3456")
+    assert parse_hand("23457") > parse_hand("23456")
+    assert parse_hand("23452") > parse_hand("A3456")
 
 
 
 def test_total_winnings():
     assert 6440 == total_winnings("""
+32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483""")
+
+
+def test_hand_type_with_joker():
+    assert parse_hand_with_joker("32T3K").hand_type == HandType.PAIR
+    assert parse_hand_with_joker("KK677").hand_type == HandType.DOUBLE_PAIR
+    assert parse_hand_with_joker("T55J5").hand_type == HandType.FOUR_OF_A_KIND
+    assert parse_hand_with_joker("KTJJT").hand_type == HandType.FOUR_OF_A_KIND
+    assert parse_hand_with_joker("QQQJA").hand_type == HandType.FOUR_OF_A_KIND
+    assert parse_hand_with_joker("JJJJJ").hand_type == HandType.FIVE_OF_A_KIND
+    assert parse_hand_with_joker("J5555").hand_type == HandType.FIVE_OF_A_KIND
+    assert parse_hand_with_joker("J2345").hand_type == HandType.PAIR
+    assert parse_hand_with_joker("J2355").hand_type == HandType.THREE_OF_A_KIND
+    assert parse_hand_with_joker("JJ232").hand_type == HandType.FOUR_OF_A_KIND
+    assert parse_hand_with_joker("J2233").hand_type == HandType.FULL_HOUSE
+
+def test_hand_comparator_with_joker():
+    assert parse_hand_with_joker("QQQJA") < parse_hand_with_joker("KTJJT")
+    assert parse_hand_with_joker("QQQJA") > parse_hand_with_joker("KK677")
+    assert parse_hand_with_joker("QQQJA") > parse_hand_with_joker("T55J5")
+    assert parse_hand_with_joker("QQQJA") > parse_hand_with_joker("32T3K")
+
+    assert parse_hand_with_joker("QQQJA") > parse_hand_with_joker("JQQQA")
+    assert parse_hand_with_joker("AJJJ2") > parse_hand_with_joker("JJJA2")
+
+
+
+
+def test_total_winnings_with_joker():
+    assert 5905 == total_winnings_with_joker("""
 32T3K 765
 T55J5 684
 KK677 28
