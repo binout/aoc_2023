10045eed944235476be65b78fa61ab8d46ea35c5
binout <benoit.prioux@gmail.com>
2023-12-10 11:39:38 +0100

    day9

--- day9.py
+++ day9.py
@@ -18,17 +18,36 @@ class OasisSensor:
 
         return list(accumulate(reversed(last_values)))[-1]
 
+    def compute_first_value(self) -> int:
+        previous_values = self.values
+        first_values = []
+        while set(previous_values) != {0}:
+            first_values.append(previous_values[0])
+            previous_values = self._compute_previous_history(previous_values)
+
+        return list(accumulate(reversed(first_values), lambda x, y: y - x))[-1]
+
     @staticmethod
     def _compute_next_history(values: List[int]) -> List[int]:
         return [values[index] - values[index-1] for index, value in enumerate(values) if index > 0]
 
+    @staticmethod
+    def _compute_previous_history(values: List[int]) -> List[int]:
+        return [values[index] - values[index-1] for index, value in enumerate(values) if index > 0]
 
-def sum_extrapolated_values(input_text:str):
+
+def sum_extrapolated_next_values(input_text:str):
     lines = [line for line in input_text.splitlines() if line.strip() != ""]
     return sum([OasisSensor([int(value) for value in line.split()]).compute_next_value() for line in lines])
 
 
+def sum_extrapolated_first_values(input_text:str):
+    lines = [line for line in input_text.splitlines() if line.strip() != ""]
+    return sum([OasisSensor([int(value) for value in line.split()]).compute_first_value() for line in lines])
+
+
 if __name__ == "__main__":
     retrieve_intput(day=9)
-    print(f"sum_extrapolated_values={sum_extrapolated_values(retrieve_intput(day=9))}")
+    print(f"sum_extrapolated_next_values={sum_extrapolated_next_values(retrieve_intput(day=9))}")
+    print(f"sum_extrapolated_first_values={sum_extrapolated_first_values(retrieve_intput(day=9))}")
 
--- test/test_day9.py
+++ test/test_day9.py
@@ -1,7 +1,7 @@
-from day9 import OasisSensor, sum_extrapolated_values
+from day9 import OasisSensor, sum_extrapolated_next_values
 
 
-def test_oasis_sensor():
+def test_oasis_sensor_next_value():
     sensor = OasisSensor([0, 3, 6, 9, 12, 15])
     assert 18 == sensor.compute_next_value()
 
@@ -9,8 +9,14 @@ def test_oasis_sensor():
     assert 28 == sensor.compute_next_value()
 
 
+def test_oasis_sensor_first_value():
+    sensor = OasisSensor([10, 13, 16, 21, 30, 45])
+    assert 5 == sensor._compute_first_value()
+
+
+
 def test_sum_extrapolated_values():
-    assert 114 == sum_extrapolated_values("""
+    assert 114 == sum_extrapolated_next_values("""
 0 3 6 9 12 15
 1 3 6 10 15 21
 10 13 16 21 30 45
