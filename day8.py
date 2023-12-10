8d7517a3ade0c5ce273dfb67c39a5fd4e887c6b3
binout <benoit.prioux@gmail.com>
2023-12-10 10:58:13 +0100

    add day8

--- day8.py
+++ day8.py
@@ -13,13 +13,22 @@ class Node:
         return self.value == "AAA"
 
     @property
+    def is_partial_start(self) -> bool:
+        return self.value.endswith("A")
+
+    @property
     def is_end(self) -> bool:
         return self.value == "ZZZ"
+
+    @property
+    def is_partial_end(self) -> bool:
+        return self.value.endswith("Z")
 
     def __repr__(self):
         return self.value
 
-@dataclass
+
+@dataclass(frozen=True)
 class Network:
     node: Node
     left: Node
@@ -54,6 +63,31 @@ def steps_to_end(input_text:str) -> int:
     return nb_steps
 
 
+def steps_to_end_with_ghost(input_text:str) -> int:
+    lines = [line for line in  input_text.splitlines() if line.strip() != ""]
+    directions = [direction for direction in lines[0].strip()]
+    networks = [_parse_network(line) for line in lines[1:] if line.strip() != ""]
+    network_by_node = {network.node: network for network in networks}
+
+    nb_steps = 0
+    current_networks = [network for network in networks if network.node.is_partial_start]
+    for direction in cycle(directions):
+        nb_steps += 1
+        for index, current_network in enumerate(current_networks):
+            if direction == "L":
+                current_networks[index] = network_by_node[current_network.left]
+            else:
+                current_networks[index] = network_by_node[current_network.right]
+        if len(current_networks) != len(set(current_networks)):
+            print(f"Reducing networks from {len(current_networks)} to {len(set(current_networks))}")
+            current_networks = list(set(current_networks))
+        if all(network.node.is_partial_end for network in current_networks):
+            break
+    return nb_steps
+
+
 if __name__ == "__main__":
     input_text = retrieve_intput(day=8)
-    print(steps_to_end(input_text))
+    print(f"steps_to_end={steps_to_end(input_text)}")
+    print(f"steps_to_end_with_ghost={steps_to_end_with_ghost(input_text)}")
+
--- test/test_day8.py
+++ test/test_day8.py
@@ -1,4 +1,4 @@
-from day8 import steps_to_end
+from day8 import steps_to_end, steps_to_end_with_ghost
 
 
 def test_steps_to_end():
@@ -21,3 +21,17 @@ AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)
     """)
+
+def test_steps_to_end_with_ghost():
+    assert 6 == steps_to_end_with_ghost("""
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+""")
